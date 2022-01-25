3d76ca3c611589bda1eecb5548779b115889b173
pitris90 <petr.balnar@seznam.cz>
2022-01-25 03:00:33 +0100

    full Brno, newer FAWE, dynmap plugin

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\Program Files\jdk-17\bin\java.exe" -Xms13G -Xmx13G -XX:+UseG1GC -jar spigot-1.17.1.jar nogui
+"C:\Program Files\jdk-17\bin\java.exe" -Xms14G -Xmx14G -XX:+UseG1GC -jar spigot-1.17.1.jar nogui
 pause
